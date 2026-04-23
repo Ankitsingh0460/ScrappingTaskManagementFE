c46d6a0ccc0dbfdd0ac5a907bdeb881441baf0f8
Ankit Singh <singhankit0460@gmail.com>
2026-04-24 00:26:39 +0530

    new

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://scrappingtaskmanagementbe-n5fm.onrender.com/api",
-  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://scrappingtaskmanagementbe-hvdv.onrender.com/api",
+  //baseURL: "http://localhost:5000/api",
   withCredentials: true,
 });
 
